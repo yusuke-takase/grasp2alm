4b7940b89c3c16ec774a5ebc05bb3a3aa321a5bb
yusuke-takase <takase_y@s.okayama-u.ac.jp>
2024-04-22 15:47:35 +0900

    add test beam files

--- grasp2alm/test/test.py
+++ grasp2alm/test/test.py
@@ -2,13 +2,9 @@ import unittest
 import grasp2alm as gp
 import numpy as np
 
-#gridfile = "../../../notebook/pix0000_119_pp_f2p2_v4_mft_tp.grd"
-#cutfile = "../../../notebook/MFT_88.5GHz_000.0_166.7_xpol_v2.cut"
-
 class TestGrasp2Alm(unittest.TestCase):
-    # TODO : add dummy file in the ./beam_files
     def test_grasp2alm_with_gridfile(self):
-        file = "../../../notebook/pix0000_119_pp_f2p2_v4_mft_tp.grd"
+        file = "./beam_files/test.grd"
         beam = gp.BeamGrid(file)
         polar = beam.to_polar()
         nside = 128
@@ -18,7 +14,7 @@ class TestGrasp2Alm(unittest.TestCase):
         self.assertTrue(np.array_equal(blm1, blm2), "blm1 and blm2 are not equal")
 
     def test_grasp2alm_with_cutfile(self):
-        file = "../../../notebook/MFT_88.5GHz_000.0_166.7_xpol_v2.cut"
+        file = "./beam_files/test.cut"
         beam = gp.BeamCut(file)
         polar = beam.to_polar()
         nside = 128
