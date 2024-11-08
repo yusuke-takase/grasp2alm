fd83cb77988830fd556095461aca0270d9c494db
yusuke-takase <takase_y@s.okayama-u.ac.jp>
2024-11-08 13:22:26 +0900

    version.py automated

--- grasp2alm/version.py
+++ grasp2alm/version.py
@@ -1,10 +1,12 @@
 # -*- encoding: utf-8 -*-
+import os
+import toml
 
-__author__ = [
-    "Yusuke Takase <takase_y@s.okayama-u.ac.jp>",
-    "Matteo Baratto <matteo.baratto@unimi.it>",
-    "Maurizio Tomasi <maurizio.tomasi@unimi.it>",
-    "Yuya Nagano <nagano_y@s.okayama-u.ac.jp>",
-    ]
+module_path = os.path.abspath(os.path.join('..','grasp2alm'))
+pyproject_path = os.path.join(module_path, 'pyproject.toml')
+with open(pyproject_path, 'r') as f:
+    pyproject_data = toml.load(f)
+
+__version__ = pyproject_data['tool']['poetry']['version']
+__author__ = pyproject_data['tool']['poetry']['authors']
 __original_url__ = "https://github.com/zonca/planck-levelS/tree/master/Beam"
-__version__ = "0.1.2"
