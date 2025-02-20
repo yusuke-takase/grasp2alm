a38f005bb6a117b14f53a0acfb97d785f82afe50
yusuke-takase <takase_y@s.okayama-u.ac.jp>
2025-02-20 17:16:03 +0900

    add elliptical gaussian function

--- grasp2alm/beam_gauss.py
+++ grasp2alm/beam_gauss.py
@@ -1,6 +1,14 @@
 import numpy as np
 import healpy as hp
 from healpy import Alm
+from typing import Optional, Union
+from scipy.special import iv as bessel_i
+
+
+def alm_index(lmax: int, ell: int, m: int) -> int:
+    "Return the index of an a_ℓm coefficient in an array"
+
+    return m * (2 * lmax + 1 - m) // 2 + ell
 
 
 class BeamGauss:
@@ -16,7 +24,7 @@ class BeamGauss:
         write2grid(self,path:str,header:str,beam): Writes the formatted beam data to the specified path with the provided headers. Each cut contains vnum lines of data.
     """
 
-    def __init__(self, amplitude: float, fwhm_deg: float):
+    def __init__(self, fwhm_deg: float, amplitude: float = 1.0):
         """
         Initializes a BeamGauss object.
 
@@ -134,41 +142,129 @@ class BeamGauss:
         self,
         lmax: int,
         mmax: int,
-        pol: bool,
+        ellipticity: Optional[float] = 1.0,
+        psi_ell_rad: Optional[float] = 0.0,
+        psi_pol_rad: Union[float, None] = 0.0,
+        cross_polar_leakage: Optional[float] = 0.0,
     ):
         """
-        Returns the spherical harmonic coefficients of the Gaussian beam.
+        Return an array of spherical harmonics :math:`a_{\ell m}` that represents a Gaussian beam
 
-        Args:
-            lmax (int): The maximum spherical harmonic degree.
-            mmax (int): The maximum spherical harmonic order.
-            pol (bool): Whether the beam is polarized.
+        :param lmax: The maximum value for :math:`\ell`
+        :param mmax: The maximum range for :math:`m`; usually this is equal to ``lmax``
+        :param ellipticity: The ellipticity of the beam, defined as major axis/minor
+        axis. Default is 1 (circular beam).
+        :param psi_ell_rad: The inclination of the major axis of the ellipse with
+        respect to the x-axis. This is not relevant for cirular beams. Default is 0.
+        :param psi_pol_rad: The polarization of the beam with respect to the x-axis,
+        if None only I beam will be returned. Default is 0 rad.
+        :param cross_polar_leakage: The cross-polar leakage (pure number). Default is 0.
 
-        Returns:
-            array: The spherical harmonic coefficients of the Gaussian beam.
+        :return:
+        :math:`a_{\ell m}` values (`numpy.ndarray`)
         """
-        ncomp = 3 if pol else 1
+        is_elliptical = False if ellipticity == 1.0 else True
+
+        is_polarized = psi_pol_rad is not None
+
+        if is_polarized:
+            num_stokes = 3
+        else:
+            num_stokes = 1
+
         nval = Alm.getsize(lmax, mmax)
-
-        assert mmax <= lmax, "mmax must be less than or equal to lmax."
-
-        blm = np.zeros((ncomp, nval), dtype=np.complex128)
+        alm = np.zeros((num_stokes, nval), dtype=np.complex128)
         fwhm_rad = np.deg2rad(self.fwhm_deg)
-        sigmasq = fwhm_rad**2 / (8.0 * np.log(2.0))
+        # alm = allocate_alm(lmax, mmax, nstokes=num_stokes)
 
-        for ell in range(0, lmax + 1):
-            blm[0, Alm.getidx(lmax, ell, 0)] = np.sqrt(
-                (2 * ell + 1) / (4.0 * np.pi)
-            ) * np.exp(-0.5 * sigmasq * ell * (ell + 1))
+        if not is_elliptical:
+            # Circular beam
+            sigma_squared = fwhm_rad**2 / (8 * np.log(2))
+            for ell in range(lmax + 1):
+                alm[0, alm_index(lmax, ell, 0)] = np.sqrt(
+                    (2 * ell + 1) / (4 * np.pi)
+                ) * np.exp(-0.5 * sigma_squared * ell * (ell + 1))
 
-        if pol:
-            for ell in range(2, lmax + 1):
-                blm[1, Alm.getidx(lmax, ell, 2)] = np.sqrt(
-                    (2 * ell + 1) / (32 * np.pi)
-                ) * np.exp(-0.5 * sigmasq * ell * (ell + 1))
-            blm[2] = 1j * blm[1]
+            if is_polarized:
+                f1 = np.cos(2 * psi_pol_rad) - np.sin(2 * psi_pol_rad) * 1.0j
+                for ell in range(2, lmax + 1):
+                    value = (
+                        np.sqrt((2 * ell + 1) / (32 * np.pi))
+                        * np.exp(-0.5 * sigma_squared * ell * (ell + 1))
+                        * f1
+                    )
+                    alm[1, alm_index(lmax, ell, 2)] = value
+                    alm[2, alm_index(lmax, ell, 2)] = value * 1.0j
+        else:
+            # Elliptical beam
+            e_squared = 1.0 - 1.0 / ellipticity**2
+            sigma_x_squared = fwhm_rad**2 * ellipticity / (np.log(2) * 8)
 
-        # Adjust normalization
-        blm[1] *= -np.sqrt(2.0)
-        blm[2] *= -np.sqrt(2.0)
-        return blm
+            # I component
+            for ell in range(lmax + 1):
+                tmp = ell * (ell + 1) * sigma_x_squared
+                for m in range(0, min(ell, mmax) + 1, 2):
+                    alm[0, alm_index(lmax, ell, m)] = (
+                        np.sqrt((2 * ell + 1) / (4 * np.pi))
+                        * np.exp(-0.5 * tmp * (1.0 - e_squared / 2))
+                        * bessel_i(m // 2, 0.25 * tmp * e_squared)
+                    )
+
+            if is_polarized:
+                # Do G and C components
+
+                rho = psi_pol_rad - psi_ell_rad
+                f1 = np.cos(2 * rho) - np.sin(2 * rho) * 1j
+                f2 = np.cos(2 * rho) + np.sin(2 * rho) * 1j
+
+                for ell in range(2, lmax + 1):
+                    tmp = ell * (ell + 1) * sigma_x_squared
+                    tmp2 = 0.25 * tmp * e_squared
+
+                    # m = 0
+                    value = (
+                        np.sqrt((2 * ell + 1) / (8 * np.pi))
+                        * np.exp(-tmp * (1.0 - e_squared / 2) / 2)
+                        * bessel_i(1, tmp2)
+                    )
+
+                    alm[1, alm_index(lmax, ell, 0)] = value * np.cos(2 * rho)
+                    alm[2, alm_index(lmax, ell, 0)] = value * np.sin(2 * rho)
+
+                    # m = 2, 4, …
+
+                    for m in range(2, min(ell, mmax) + 1, 2):
+                        value = np.sqrt((2 * ell + 1) / (8 * (4 * np.pi))) * np.exp(
+                            -0.5 * tmp * (1.0 - 0.5 * e_squared)
+                        )
+                        b1 = f1 * bessel_i((m - 2) // 2, tmp2)
+                        b2 = f2 * bessel_i((m + 2) // 2, tmp2)
+                        alm[1, alm_index(lmax, ell, m)] = value * (b1 + b2)
+                        alm[2, alm_index(lmax, ell, m)] = value * (b1 - b2) * 1j
+
+            # Rotate the multipoles through angle psi_ell about the z-axis, so
+            # the beam is in the right orientation (only need this for even m).
+
+            for m in range(0, mmax + 1, 2):
+                f1 = np.cos(m * psi_ell_rad) - np.sin(m * psi_ell_rad) * 1j
+                for n in range(num_stokes):
+                    for ell in range(m, lmax + 1):
+                        alm[n, alm_index(lmax, ell, m)] *= f1
+
+        # Adjust multipoles for cross-polar leakage
+        alm[0, :] *= 1.0 + cross_polar_leakage
+
+        if num_stokes > 1:
+            for n in (1, 2):
+                alm[n, :] *= 1.0 - cross_polar_leakage
+
+        # Adjust the normalization
+        if num_stokes > 1:
+            for n in (1, 2):
+                alm[n, :] *= -np.sqrt(2.0)
+        self.alm = alm
+        return alm
+
+    def get_profile(self, nside):
+        assert hasattr(self, 'alm'), "Please run get_alm() first"
+        return hp.alm2map(self.alm, nside)
---
+++ test/test_beam_gauss.py
@@ -0,0 +1,61 @@
+import unittest
+import os
+from pathlib import Path
+import numpy as np
+from grasp2alm import BeamGauss
+
+
+class TestBeamCut(unittest.TestCase):
+    """
+    Testing the functionality of converting a gaussian beam
+    to alm coefficients using the grasp2alm function.
+
+    Methods:
+        setUp():
+            Initializes the test environment. This includes setting up parameters
+            for the beam and writing the beam cut file.
+
+        test_get_alm():
+            Tests the get_alm method of the BeamGauss class.
+    """
+
+    def setUp(self):
+        """
+        Sets up the parameters for the Gaussian beam.
+        """
+        self.path_gauss_alm: str = str(
+            Path(__file__).parent / "reference" / "gauss_alm.npy"
+        )
+        self.lmax = 500
+        self.mmax = self.lmax
+        self.fwhm_deg = 1.0
+
+        self.ellipticity = 0.5
+        self.psi_ell_rad = np.pi / 6
+        self.psi_pol_rad = np.pi / 4
+        self.cross_polar_leakage = 0.0
+
+    def test_get_alm(self, save: bool = False):
+        """
+        Tests the get_alm method of the BeamGauss class.
+
+        Args:
+            save (bool): If True, the generated alm coefficients are saved to a file.
+        """
+        Gauss = BeamGauss(self.fwhm_deg)
+        alm = Gauss.get_alm(
+            self.lmax,
+            self.mmax,
+            self.ellipticity,
+            self.psi_ell_rad,
+            self.psi_pol_rad,
+            self.cross_polar_leakage,
+        )
+        if save:
+            np.save(self.path_gauss_alm, alm)
+        else:
+            alm_ref = np.load(self.path_gauss_alm)
+            self.assertTrue(
+                np.allclose(alm, alm_ref),
+                "Generated alm coefficients do not match reference values.",
+            )
